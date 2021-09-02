922bda093a0d940e29986dc2481faf43aa8e7a1e
Sridhar Rajaram <srilumia@hotmail.com>
2021-09-03 00:05:27 +0530

    error

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/users",async function(req,res){
         res.json(data) //reply with data
 
     } catch (error) {
-        console.log(error.res.data);
+        console.log(error);
         res.status(500).json({
             message: "something went wrong"
         })
