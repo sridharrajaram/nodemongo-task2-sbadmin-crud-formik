6a07d378bd1beb92a957ad7038a78c9a6cad8883
Sridhar Rajaram <srilumia@hotmail.com>
2021-09-03 00:08:33 +0530

    pwd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 
-const url = "mongodb+srv://sridharrajaram:mdBsb@dmin@cluster0.eejhy.mongodb.net?retryWrites=true&w=majority"
+const url = "mongodb+srv://sridharrajaram:mdBsadmin@cluster0.eejhy.mongodb.net?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 3001
 
